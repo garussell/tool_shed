9e3045f88483afd595e12f0c549b395364143b78
Allen Russell <garyrusselljr@gmail.com>
2023-07-06 12:13:10 -0600

    Add routes for tools and tools/:id

--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,16 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "sheds#index"
 
 # sheds 
   get "/sheds", to: "sheds#index"
   get "/sheds/:id", to: "sheds#show"
 
 # tools
-  get "/sheds/:id/tools", to: "shed_tools#index"
+  get "/tools", to: "tools#index"
+  get "/tools/:id", to: "tools#show"
   
+  get "/sheds/:id/tools", to: "shed_tools#index"
+  get "/sheds/:id/tools/:id", to: "shed_tools#show"
 end
