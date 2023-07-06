6ae05afa3a2abad84b47883ded034eec05cfcea5
Allen Russell <garyrusselljr@gmail.com>
2023-07-06 09:01:45 -0600

    Create route for shed tools controller

--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,11 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
 
-  get "/", to: "sheds#index"
-
+# sheds 
   get "/sheds", to: "sheds#index"
-  get "/sheds/new", to: "sheds#new"
   get "/sheds/:id", to: "sheds#show"
+
+# tools
+  get "/sheds/:id/tools", to: "shed_tools#index"
+  
 end
