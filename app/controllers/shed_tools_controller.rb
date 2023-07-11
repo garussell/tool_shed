c5dcb30a060a4b2bd9a9bd638da3d8429618d11d
Allen Russell <garyrusselljr@gmail.com>
2023-07-10 18:10:29 -0600

    refactor: Add return to index link to application.html and removed from other views

--- app/controllers/shed_tools_controller.rb
+++ app/controllers/shed_tools_controller.rb
@@ -7,10 +7,21 @@ class ShedToolsController < ApplicationController
   def new
   end
 
+  
   def create
     shed = Shed.find(params[:id])
     tool = shed.tools.create(tool_params)
     redirect_to "/sheds/#{shed.id}/tools"
+  end
+  
+  def edit
+    @tools = Tool.find(params[:id])
+  end
+  
+  def update
+    tool = Tool.find(params[:id])
+    tool.update(tool_params)
+    redirect_to "/tools/#{tool.id}"
   end
 
   private
